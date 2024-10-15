aabaf6dfe0d71e42f250215accebbe98be8e5826
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-15 16:12:06 +0530

    Install react-router-dom

--- Admin-Store/frontend/src/main.jsx
+++ Admin-Store/frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
 
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
+      <BrowserRouter>
       <App />
+      </BrowserRouter>
     </ChakraProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
 
