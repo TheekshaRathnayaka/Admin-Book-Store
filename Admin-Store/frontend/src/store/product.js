a9b95749f251591923d09302163daefd999ffb99
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-17 02:01:28 +0530

    Async function for updateProduct

--- Admin-Store/frontend/src/store/product.js
+++ Admin-Store/frontend/src/store/product.js
@@ -50,5 +50,28 @@ export const useProductStore = create((set) => ({
     },
 
 
-    
-}))
+    updateProduct: async (pid, updatedProduct) => {
+
+        const res = await fetch(`/api/products/${pid}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedProduct),
+        });
+
+        const data = await res.json();
+
+        if (!data.success)
+            return { success: false, message: data.message };
+
+        set((state) => ({
+            products: state.products.map((product) => (product._id === pid? data.data : product)),
+        }));
+
+        return { success: true, message: data.message };
+    },
+
+
+}));
+
