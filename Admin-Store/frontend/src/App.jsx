0ace2338ac825f14bf17ae6a26f1eda1624ae54d
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-17 13:14:13 +0530

    Create home page for bookstore

--- Admin-Store/frontend/src/App.jsx
+++ Admin-Store/frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
 
       <Navbar />
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- Admin-Store/frontend/src/store/product.js
+++ Admin-Store/frontend/src/store/product.js
@@ -23,7 +23,7 @@ export const useProductStore = create((set) => ({
 
         const data = await res.json();
         set((state) => ({ products: [...state.products, data.data] }));
-        return { success: true, message: "Product created successfully." };
+        return { success: true, message: "Book added successfully." };
     },
 
 
