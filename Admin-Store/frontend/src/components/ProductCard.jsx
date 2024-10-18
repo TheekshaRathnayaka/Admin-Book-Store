c863480d4b6fb10b3a595ff1349d6bce658c65be
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-10-19 00:56:58 +0530

    Add params to update function

--- Admin-Store/frontend/src/components/ProductCard.jsx
+++ Admin-Store/frontend/src/components/ProductCard.jsx
@@ -57,7 +57,7 @@ const ProductCard = ({ product }) => {
 		}
 	};
 
-	const handleUpdateProduct = async () => {
+	const handleUpdateProduct = async (pid, updatedProduct) => {
 		const { success, message } = await updateProduct(pid, updatedProduct);
 		onClose();
         if (!success) {
