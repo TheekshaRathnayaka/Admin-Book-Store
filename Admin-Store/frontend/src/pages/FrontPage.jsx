4faf37457cea9b33fd813ef4bb6fa4fa761be337
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-12-28 23:46:58 +0530

    Add background image

--- Admin-Store/frontend/src/pages/FrontPage.jsx
+++ Admin-Store/frontend/src/pages/FrontPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Box, Button, Container, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const FrontPage = () => {
   return (
-    <div>
+    <Box
+      height="100vh"
+      backgroundImage="url('https://example.com/background-image.jpg')"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      filter="blur(2px)"
+    >
       
-    </div>
-  )
-}
+    </Box>
+  );
+};
 
-export default FrontPage
+export default FrontPage;
