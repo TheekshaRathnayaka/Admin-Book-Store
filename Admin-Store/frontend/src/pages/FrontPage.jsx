1293e9b0c8b08d9c825da3ceff7570dd0166aea4
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2024-12-29 20:10:57 +0530

    Add front page topic

--- Admin-Store/frontend/src/pages/FrontPage.jsx
+++ Admin-Store/frontend/src/pages/FrontPage.jsx
@@ -6,12 +6,33 @@ const FrontPage = () => {
   return (
     <Box
       height="100vh"
-      backgroundImage="url('https://example.com/background-image.jpg')"
+      backgroundImage="url('https://www.freepik.com/free-photo/front-view-books-with-glasses_5207434.htm#fromView=search&page=1&position=40&uuid=8a94b0fb-7fe0-4847-ab77-35a1c3e08460&new_detail=true')"
       backgroundSize="cover"
       backgroundPosition="center"
       filter="blur(2px)"
     >
-      
+    <Box
+        height="100vh"
+        position="relative"
+        backgroundColor="rgba(0, 0, 0, 0.6)"
+        color="white"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+    >
+    <Container maxW="container.md" textAlign="center">
+      <VStack spacing={6}>
+        <Text
+          fontSize="4xl"
+          fontWeight="bold"
+          bgGradient="linear(to-r, cyan.400, blue.500)"
+          bgClip="text"
+        >
+          Welcome to the Admin Book Store 📚
+        </Text>
+      </VStack>
+    </Container>
+    </Box>
     </Box>
   );
 };
