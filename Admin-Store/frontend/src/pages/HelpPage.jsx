ffcab8c4429422430e2c94bd4e796d11569b0e29
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2025-01-09 00:10:45 +0530

    Add new navbar to help page

--- Admin-Store/frontend/src/pages/HelpPage.jsx
+++ Admin-Store/frontend/src/pages/HelpPage.jsx
@@ -1,12 +1,14 @@
 import { Box, Container, Heading, VStack } from '@chakra-ui/react'
 import React from 'react'
+import MainNavbar from '../components/MainNavbar'
 
 const HelpPage = () => {
   return (
-    <Box bg="gray.50" minHeight="100vh" py={10} px={5}>
+    <Box bg="gray.50" minHeight="100vh" py={20} px={5}>
+      <MainNavbar />
       <Container maxW="container.lg">
         <VStack spacing={6} mb={10} textAlign="center">
-           <Heading fontSize="4xl"
+           <Heading fontSize="5xl"
                     bgGradient="linear(to-r, cyan.400, blue.500)"
                     bgClip="text">
               Help & Support
