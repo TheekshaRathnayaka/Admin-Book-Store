b8c337590f515432b781fe2625fb623b97db920e
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2025-01-08 23:32:43 +0530

    Add a heading

--- Admin-Store/frontend/src/pages/HelpPage.jsx
+++ Admin-Store/frontend/src/pages/HelpPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, VStack } from '@chakra-ui/react'
+import { Box, Container, Heading, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const HelpPage = () => {
@@ -6,7 +6,11 @@ const HelpPage = () => {
     <Box bg="gray.50" minHeight="100vh" py={10} px={5}>
       <Container maxW="container.lg">
         <VStack spacing={6} mb={10} textAlign="center">
-           
+           <Heading fontSize="4xl"
+                    bgGradient="linear(to-r, cyan.400, blue.500)"
+                    bgClip="text">
+              Help & Support
+           </Heading>
         </VStack>
       </Container>
     </Box>
