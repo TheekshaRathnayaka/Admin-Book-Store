024b5d7bf63124d0e7bc332c39130f1722dcf943
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2025-01-08 23:23:12 +0530

    Set container for FAQ

--- Admin-Store/frontend/src/pages/HelpPage.jsx
+++ Admin-Store/frontend/src/pages/HelpPage.jsx
@@ -1,10 +1,15 @@
+import { Box, Container, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const HelpPage = () => {
   return (
-    <div>
-      
-    </div>
+    <Box bg="gray.50" minHeight="100vh" py={10} px={5}>
+      <Container maxW="container.lg">
+        <VStack spacing={6} mb={10} textAlign="center">
+           
+        </VStack>
+      </Container>
+    </Box>
   )
 }
 
