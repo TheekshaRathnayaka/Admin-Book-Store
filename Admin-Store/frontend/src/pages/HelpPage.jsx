a91606ee268986fed4b7b69db5f0fdb5dbea0193
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2025-01-10 19:45:02 +0530

    Add FAQ heading

--- Admin-Store/frontend/src/pages/HelpPage.jsx
+++ Admin-Store/frontend/src/pages/HelpPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Heading, VStack } from '@chakra-ui/react'
+import { Box, Container, Heading, VStack, Text } from '@chakra-ui/react'
 import React from 'react'
 import MainNavbar from '../components/MainNavbar'
 
@@ -13,7 +13,13 @@ const HelpPage = () => {
                     bgClip="text">
               Help & Support
            </Heading>
+           <Text fontSize="2xl" color="gray.600">
+            Find answers to common questions and learn how to manage your book store effectively.
+           </Text>
         </VStack>
+        <Heading size="lg" mb={6} color="blue.600">
+          Frequently Asked Questions
+        </Heading>
       </Container>
     </Box>
   )
