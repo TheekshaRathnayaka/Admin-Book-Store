c0e62d96ace2ab70aac4062c7480068aa5e9dcc4
TheekshaRathnayaka <jrntheeksha@std.appsc.sab.ac.lk>
2025-01-13 22:29:40 +0530

    Make screen responsive

--- Admin-Store/frontend/src/pages/HelpPage.jsx
+++ Admin-Store/frontend/src/pages/HelpPage.jsx
@@ -9,12 +9,12 @@ const HelpPage = () => {
       <MainNavbar />
       <Container maxW="container.lg">
         <VStack spacing={6} mb={10} textAlign="center">
-           <Heading fontSize="5xl"
+           <Heading fontSize={{ base: "3xl", md:"5xl" }}
                     bgGradient="linear(to-r, cyan.400, blue.500)"
                     bgClip="text">
               Help & Support
            </Heading>
-           <Text fontSize="2xl" color="gray.600">
+           <Text fontSize={{ base: "md", md: "2xl" }} color="gray.600">
             Find answers to common questions and learn how to manage your book store effectively.
            </Text>
         </VStack>
@@ -25,41 +25,41 @@ const HelpPage = () => {
 
           <AccordionItem>
             <h2>
-              <AccordionButton fontSize="lg" fontWeight="bold" p={5} _expanded={{ bg: "blue.50" }}>
+              <AccordionButton fontSize={{ base: "md", md: "lg" }} fontWeight="bold" p={5} _expanded={{ bg: "blue.50" }}>
                 <Box flex="1" textAlign="left">
                   How do I add a new book to the store?
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
             </h2>
-            <AccordionPanel pb={4} fontSize="18px">
+            <AccordionPanel pb={4} fontSize={{ base: "sm", md: "md" }}>
               To add a new book, go to the "Store" section and click on the "+ icon" button. Fill out the form with the book's details, then click "Add Book".
             </AccordionPanel>
           </AccordionItem>
 
           <AccordionItem>
             <h2>
-              <AccordionButton fontSize="lg" fontWeight="bold" p={5} _expanded={{ bg: "blue.50" }}>
+              <AccordionButton fontSize={{ base: "md", md: "lg" }} fontWeight="bold" p={5} _expanded={{ bg: "blue.50" }}>
                 <Box flex="1" textAlign="left">
                   How do I edit book details?
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
             </h2>
-            <AccordionPanel pb={4} fontSize="18px">
+            <AccordionPanel pb={4} fontSize={{ base: "sm", md: "md" }}>
               Locate the book in the "Store" section, click on the "Edit icon" button, change the necessary details, and "Update" your changes.            </AccordionPanel>
           </AccordionItem>
 
           <AccordionItem>
             <h2>
-              <AccordionButton fontSize="lg" fontWeight="bold" p={5} _expanded={{ bg: "blue.50" }}>
+              <AccordionButton fontSize={{ base: "md", md: "lg" }} fontWeight="bold" p={5} _expanded={{ bg: "blue.50" }}>
                 <Box flex="1" textAlign="left">
                   How do I delete a book from the store?
                 </Box>
                 <AccordionIcon />
               </AccordionButton>
             </h2>
-            <AccordionPanel pb={4} fontSize="18px">
+            <AccordionPanel pb={4} fontSize={{ base: "sm", md: "md" }}>
               Find the book you want to remove in the "Store" section, then click the "Delete" button.
             </AccordionPanel>
           </AccordionItem>
